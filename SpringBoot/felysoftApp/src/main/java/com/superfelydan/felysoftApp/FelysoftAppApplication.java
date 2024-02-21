992f74fa26acf99a6a7340dcf540a4396721b60d
MiguelC29 <miguelito28c@gmail.com>
2024-02-20 22:10:31 -0500

    Añadiendo proyecto springboot_clase

---
+++ SpringBoot/felysoftApp/src/main/java/com/superfelydan/felysoftApp/FelysoftAppApplication.java
@@ -0,0 +1,12 @@
+package com.superfelydan.felysoftApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelysoftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelysoftAppApplication.class, args);
+	}
+}
---
+++ SpringBoot/felysoftApp/src/test/java/com/superfelydan/felysoftApp/FelysoftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superfelydan.felysoftApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelysoftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
